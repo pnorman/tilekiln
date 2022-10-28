64a0147844b7a487d41cf70a816f6754e7a86305
Paul Norman <penorman@mac.com>
2022-10-27 20:20:23 -0700

    Make DB connections read-only

--- tilekiln/kiln.py
+++ tilekiln/kiln.py
@@ -3,9 +3,14 @@ The kiln is what actually generates the tiles, using the config to compute SQL,
 and a DB connection to execute it
 '''
 from tilekiln.config import Config
+import psycopg
 
 
 class Kiln:
-    def __init__(self, config: Config, connection):
+    def __init__(self, config: Config, connection: psycopg.Connection):
         self.__config = config
         self.__connection = connection
+
+        # New connection setup
+        self.__connection.execute('''SET default_transaction_read_only = true;''')
+        self.__connection.commit()
