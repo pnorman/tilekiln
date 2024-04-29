b2f8161d272cf2a644e989ed794b21edfac397f0
Paul Norman <penorman@mac.com>
2024-04-29 16:33:45 -0700

    Correct source/storage flip

--- tilekiln/scripts/generate.py
+++ tilekiln/scripts/generate.py
@@ -84,12 +84,12 @@ def zooms(config: int, num_threads: int,
     tiles = Tilerange(min_zoom, max_zoom)
     threads = min(num_threads, len(tiles))  # No point in more threads than tiles
     click.echo(f"Rendering {len(tiles)} tiles over {threads} threads")
-    source_kwargs = {"dbname": storage_dbname,
-                     "host": storage_host,
-                     "port": storage_port,
-                     "user": storage_username}
-    storage_kwargs = {"dbname": source_dbname,
+    source_kwargs = {"dbname": source_dbname,
                       "host": source_host,
                       "port": source_port,
                       "user": source_username}
+    storage_kwargs = {"dbname": storage_dbname,
+                     "host": storage_host,
+                     "port": storage_port,
+                     "user": storage_username}
     tilekiln.generator.generate(c, source_kwargs, storage_kwargs, tqdm(tiles), threads)
